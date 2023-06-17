5706efa57e20d08a21d54b6376f145d98e375da7
Rafiqul Islam <rafiqr169q@gmail.com>
2023-06-18 01:25:27 +0200

    Create api client to send api request

---
+++ src/api-client.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}/`,
+});
+
+// Intercet request to add authorization token in header before request send
+apiClient.interceptors.request.use((config) => {
+  const token = localStorage.getItem("ACCESS_TOKEN");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// Intercet response to handle the errors
+apiClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const { response } = error;
+    if (response.status === 401) {
+      localStorage.removeItem("ACCESS_TOKEN");
+      console.log(response);
+    } else if (response.status === 404) {
+      console.log(response);
+    }
+
+    throw error;
+  }
+);
+
+export default apiClient;
