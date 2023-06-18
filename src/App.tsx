fa77304e3d27a586452ad9c38d3c868ddbef69c0
Rafiqul Islam <rafiqr169q@gmail.com>
2023-06-18 16:35:42 +0200

    Structure all component and include in news feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
-import { NavLink } from "react-router-dom";
-import NewsFeed from "./pages/news-feed";
 import "./App.css";
-import { Button } from "@mui/material";
-import { useAuthContext } from "./auth/auth-context";
+import "./style.css";
+
+import upArrowIcon from "./assets/up-arrow.svg";
+import { useCustomJs } from "./hooks/use-custom-js";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import MobileMenu from "./components/mobile-menu/mobile-menu";
+import NewsSection from "./components/feed/news-section";
+import articles from "./components/feed/articles.json";
+import NewsSlider from "./components/feed/news-slider/news-slider";
+import Category from "./components/feed/category/category";
 
 function App() {
-  const { logout } = useAuthContext();
-
+  useCustomJs();
   return (
-    <>
-      <NewsFeed />
-      <NavLink to="/signin">Sign In</NavLink>
-      <Button variant="text" onClick={logout}>
-        Log out
-      </Button>
-    </>
+    <div className="text-gray-700 pt-9 sm:pt-10">
+      <Header />
+
+      <MobileMenu />
+
+      <main id="content">
+        <Category />
+
+        <NewsSection
+          categoryName="Latest News"
+          articles={articles}
+          hasFeatureNews={true}
+        />
+
+        <NewsSlider />
+
+        <NewsSection
+          categoryName="Bussiness"
+          articles={articles}
+          hasFeatureNews={false}
+        />
+
+        <NewsSection
+          categoryName="Entertainment"
+          articles={articles}
+          hasFeatureNews={false}
+        />
+      </main>
+
+      <Footer />
+
+      <a
+        href="#"
+        className="back-top fixed p-4 rounded bg-gray-100 border border-gray-100 text-gray-500 dark:bg-gray-900 dark:border-gray-800 right-4 bottom-4 hidden"
+      >
+        <img src={upArrowIcon} alt="Up" />
+      </a>
+    </div>
   );
 }
 
