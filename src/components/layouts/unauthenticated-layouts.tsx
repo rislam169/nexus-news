47110daf8a84e52f9bd0aa1111d750e3756cb12e
Rafiqul Islam <rafiqr169q@gmail.com>
2023-06-18 16:36:13 +0200

    Add utils function and remove unused code from layouts

--- src/components/layouts/unauthenticated-layouts.tsx
+++ src/components/layouts/unauthenticated-layouts.tsx
@@ -2,7 +2,7 @@ import { useAuthContext } from "../../auth/auth-context";
 import { Navigate, Outlet } from "react-router-dom";
 
 export default function UnAuthenticatedLayouts() {
-  const { user, token } = useAuthContext();
+  const { token } = useAuthContext();
 
   if (token) {
     return <Navigate to="/" />;
